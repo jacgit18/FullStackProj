5d5a0d8d7e1da7ffd33340d8e028a673bd0089cd
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-24 18:02:43 -0500

    clean up

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,31 +1,38 @@
 #!/user/bin/env node
 import cors from "cors"
 import express from "express"
-
-// import config from "../config/config.js"
+import morgan from "morgan"
+import config from "../config/config.js"
 // import { initPerformanceMonitoring } from "../libs/monitoring.js"
+
 
 const app = express()
 // NOTE: must be first
 // app.use(initPerformanceMonitoring)
 
 // Middle Wares
-// if (!config.TESTING) {
+if (!config.TESTING) {
 // dont need logging if we are in testing environment
-//   app.use(morgan("tiny"))
-// }
+  app.use(morgan("tiny"))
+}
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 // app.use(convertQueryOperators)
 // app.use(companyIdFromHeaders)
 
+
+
 // Unauthenticated user log in routes
 // app.use( "/v2", routes.authRouter)
+
+
 
 // Authenticated everything else
 // app.all("/v1", authenticateToken, routes.apiPassthroughRouter)
 // app.use("/v2", authenticateToken, routes.letterRouter)
+
+
 
 app.get("/", (req, res) => {
   res.send("Hello from the backend!")
